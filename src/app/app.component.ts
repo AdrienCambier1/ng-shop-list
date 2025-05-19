7639357505a054a0ba4c2353155ff45a2b394b36
Cambier-Adrien <adrien.cambier04@gmail.com>
2025-05-19 12:36:14 +0200

    ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Router, NavigationEnd } from '@angular/router';
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
     <app-header />
-    <main class="min-h-screen !pt-16 p-6">
+    <main class="min-h-screen !pt-16 px-6">
       <router-outlet />
     </main>
     <app-footer />
